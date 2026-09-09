2d7733bcb749e3550094d7b5e62134a63fdde578
juhwan7 <43717710+juhwan7@users.noreply.github.com>
2026-09-09 16:41:08 +0900

    refactor: 국내 SaaS 보조 로직의 전역 마운트 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import ThemeFlowSaasEnhancer from './ThemeFlowSaasEnhancer'
 import './styles.css'
 import './themeFlowSaas.css'
 
@@ -14,6 +13,5 @@ if ('serviceWorker' in navigator) {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <ThemeFlowSaasEnhancer />
   </React.StrictMode>,
 )
